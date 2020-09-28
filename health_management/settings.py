b6877441ce7921a9d26e3e037613b345e1c9975b
dobbiedts <64968262+dobbiedts@users.noreply.github.com>
2020-09-28 12:38:05 +0100

    Update settings.py

--- health_management/settings.py
+++ health_management/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'hosmanagement.herokuapp.com']
 
 
 # Application definition
